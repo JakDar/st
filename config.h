ea3fad9ee8937e31a1dbc42144d7696fd7f2cd86
Jakub Darul <daruljakub@protonmail.com>
2020-05-03 20:22:25 +0200

    fix: emoji font

--- config.h
+++ config.h
@@ -8,7 +8,7 @@
 static char *font = "mono:pixelsize=16:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"Inconsolata for Powerline:pixelsize=16:antialias=true:autohint=true",
+	"Noto Color Emoji:pixelsize=16:antialias=true:autohint=true", // requires `yay -S libxft-bgra` on arch
 	/* "Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true" */
 };
 
